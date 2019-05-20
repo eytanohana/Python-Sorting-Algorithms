1abd775b92eb0a9e7912673e498bc43c27257205
eytanohana <eohana98@gmail.com>
2019-05-21 02:41:04 +0300

    Debugging merge sort

--- src/Sorting.py
+++ src/Sorting.py
@@ -1,10 +1,12 @@
 
 
 
-def bubbleSort(ls):
+def bubble_sort(ls):
     """
-    The Bubble Sort algorithm with the optimization that if
+    My implementation of the Bubble Sort algorithm with the optimization that if
     it traverses the list once without swapping at all the function ends.
+    The algorithm works by constantly "bubbling" the largest value to the ebd of the list
+    Runs in O(n^2).
 
     :param ls: The list to sort
     :return: The list sorted
@@ -15,8 +17,14 @@ def bubbleSort(ls):
                 ls[i], ls[i+1] = ls[i+1], ls[i]
     return ls
 
-def selectionSort(ls):
+def selection_sort(ls):
     """
+    My implementation of the Selection Sort algorithm.
+    The algorithm works by finding the minimum in the list and swapping it
+    with the 0'th element. Then finding the next minimum and swapping it with the first
+    and so on.
+    Runs in O(n^2).
+
     :param ls: The list to sort
     :return: The list sorted
     """
@@ -32,8 +40,13 @@ def selectionSort(ls):
     return ls
 
 
-def insertionSort(ls):
+def insertion_sort(ls):
     """
+    My implementation of the Insertion Sort algorithm.
+    The algorithm works by starting at the beginning of the list
+    and bringing smaller elements towards the beginning.
+    Runs in O(n^2).
+
     :param ls: The list to sort
     :return: The list sorted
     """
@@ -45,14 +58,46 @@ def insertionSort(ls):
     return ls
 
 
+def merge_sort(ls):
+    def merge_sort(ls, begin_index, end_index):
+        if begin_index < end_index:
+            mid_index = (begin_index + end_index) // 2
+            merge_sort(ls, begin_index, mid_index)
+            merge_sort(ls, mid_index+1, end_index)
+            merge(ls, begin_index, mid_index, end_index)
+
+    def merge(ls, begin_index, mid_index, end_index):
+        left_len = mid_index - begin_index + 1
+        right_len = end_index - mid_index
+        left = []
+        right = []
+        for i in range(left_len):
+            left.append(ls[i])
+        for i in range(right_len):
+            right.append(ls[mid_index+1+i])
+
+        i = 0
+        j = 0
+        for k in range(begin_index, end_index-1):
+            if left[i] <= right[j]:
+                ls[k] = left[i]
+                i += 1
+            else:
+                ls[k] = right[j]
+                j += 1
+
+
+    return merge_sort(ls, 0, len(ls)-1)
+
+
 
 
 
 
 
 if __name__ == '__main__':
-    ls = [1,0]
+    ls = [5,4,3,2,1]
     print(ls)
 
-    ls = insertionSort(ls)
+    ls = merge_sort(ls)
     print(ls)
