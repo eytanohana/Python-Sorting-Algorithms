368317909eff4898c5ec750e51d0d734f60353d2
Eytan Ohana <eohana98@gmail.com>
2019-05-22 01:00:14 +0300

    Changed random function

--- src/SortingComparator.py
+++ src/SortingComparator.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
+from numpy.random import normal
 from src.Sorting import *
-from random import randint
 from time import time
 
-def compareAlgorithms(sort1, sort2):
+def compareAlgorithms(sort1, sort2, num_tests=15):
     """
     This function plots the amount of time it takes for two different
     sorting algorithms to run as a function of the size of their input lists.
@@ -16,17 +16,16 @@ def compareAlgorithms(sort1, sort2):
     # to sort both lists using each algorithm
     sort1_times = []
     sort2_times = []
-    NUM_TESTS = 12
     # We create NUM_TESTS lists of random
     # numbers to sort.
-    for i in range(NUM_TESTS):
-        print(f"{i}", end=' ')
+    for i in range(num_tests):
+        print(f"Test {i}...")
         a = []
         b = []
         # In each successive iteration, we double
         # the size of the lists.
         for j in range(2**i):
-            a.append(randint(1, 100))
+            a.append(normal(50, 25))
             b.append(a[-1])
 
         # get the time it takes to
@@ -48,7 +47,8 @@ def compareAlgorithms(sort1, sort2):
     # We make a dictionary to hold the names of the algorithms
     # for labeling the graph.
     algorithms = {bubble_sort : "Bubble Sort", selection_sort : "Selection Sort",
-                  insertion_sort : "Insertion Sort", merge_sort : "Merge Sort"}
+                  insertion_sort : "Insertion Sort", merge_sort : "Merge Sort",
+                  rand_quick_sort : "Random Pivot Quick Sort"}
 
     for k,v in algorithms.items():
         if sort1 is k:
@@ -57,15 +57,12 @@ def compareAlgorithms(sort1, sort2):
             label2 = v
 
     # plot the graph
-    plt.plot([2**i for i in range(NUM_TESTS)], sort1_times, color='red', label=label1)
-    plt.plot([2**i for i in range(NUM_TESTS)], sort2_times, color='blue', label=label2)
+    plt.plot([2**i for i in range(num_tests)], sort1_times, color='red', label=label1)
+    plt.plot([2**i for i in range(num_tests)], sort2_times, color='blue', label=label2)
     plt.xlabel('List Size')
     plt.ylabel('Time (ms) to sort')
     plt.legend()
     plt.show()
 
 if __name__ == '__main__':
-    # compareAlgorithms(bubble_sort, merge_sort)
-    ls = [4,2,5,7,3,7,4,32,5,12,7,3,0,1,2,5,3,4,5]
-    rand_quick_sort(ls)
-    print(ls)
+    compareAlgorithms(rand_quick_sort, merge_sort)
