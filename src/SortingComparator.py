9d9743916b05b586e93a6614e473ca6dc009e100
Eytan Ohana <eohana98@gmail.com>
2019-05-21 22:16:45 +0300

    Working on comparator

--- src/SortingComparator.py
+++ src/SortingComparator.py
@@ -3,12 +3,13 @@ from src.Sorting import *
 from random import randint
 from time import time
 
-NUM_OF_TESTS = 16
+NUM_OF_TESTS = 13
 
 def compareAlgorithms(sort1, sort2):
     sort1_times = []
     sort2_times = []
     for i in range(NUM_OF_TESTS):
+        print(f"{i}", end=' ')
         a = []
         b = []
         for j in range(2**i):
@@ -18,20 +19,25 @@ def compareAlgorithms(sort1, sort2):
         start_time = time() * 1000
         sort1(a)
         end_time = time() * 1000
+        sort1_times.append(end_time - start_time)
+
+        start_time = time() * 1000
+        sort2(b)
+        end_time = time() * 1000
+        sort2_times.append(end_time - start_time)
 
 
 
-
-
-
+    plt.plot([2**i for i in range(NUM_OF_TESTS)], sort1_times, color='red', label="First")
+    plt.plot([2**i for i in range(NUM_OF_TESTS)], sort2_times, color='blue', label='Second')
+    plt.xlabel('List Size')
+    plt.ylabel('Time (ms) to sort')
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
-    list1 = []
+    compareAlgorithms(bubble_sort, insertion_sort)
 
-    for i in range(10):
-        list1.append(randint(1, 100))
-    print(list1)
 
-    bubble_sort(list1)
-    print(list1)
-    print(time()*1000)
+
+
