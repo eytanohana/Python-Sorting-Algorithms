c1acc82f937540800bb7063c8c737f640271cfea
eytanohana <eohana98@gmail.com>
2025-12-27 01:23:52 +0200

    add tests

---
+++ tests.py
@@ -0,0 +1,26 @@
+from collections.abc import Callable
+
+import pytest
+
+from Sorting import bubble_sort, insertion_sort, selection_sort, merge_sort, rand_quick_sort, median3_quicksort
+import numpy as np
+
+
+np.random.seed(1)
+
+
+
+@pytest.mark.parametrize('sorting_algo', [
+    pytest.param(bubble_sort, id='bubble_sort'),
+    pytest.param(insertion_sort, id='insertion_sort'),
+    pytest.param(selection_sort, id='selection_sort'),
+    pytest.param(merge_sort, id='merge_sort'),
+    pytest.param(rand_quick_sort, id='rand_quick_sort'),
+    pytest.param(median3_quicksort, id='median3_quicksort'),
+])
+def test_sorting_algorithm(sorting_algo: Callable[[list[int]], [None]]):
+    ls = np.random.randint(0,100, size=100)
+    expected = np.sort(ls)
+    print(ls)
+    sorting_algo(ls)
+    assert np.all(ls == expected)
